f7197ac7a064e81c97304d1fb3089d259fa4f6cb
EC2 Default User <ec2-user@ip-172-31-80-112.ec2.internal>
2019-11-22 07:34:31 +0000

    lab 9

--- app.js
+++ app.js
@@ -7,35 +7,47 @@ app.use(express.static("assets")); //folder for images, css, js
 const request = require('request');
 const mysql = require('mysql');
 
-//routes
-app.get("/", function(req, res){
-    res.render("index");
+
+
+
+
+/* **********ROUTES********** */
+
+app.get("/", async function(req, res){
+    let categories = await getCategories();
+    let authors = await getAuthors();
+    
+    res.render("index", {"categories":categories, "authors":authors});
 }); //root route
 
-app.get("/quotes", function(req, res){
-
-    let conn = dbConnection();
+app.get("/quotes", async function(req, res){
     
-    conn.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        
-        let keyword = req.query.searchTerm;
-        let sql = `
-            SELECT quote, lastName, category FROM q_quotes
-            NATURAL JOIN q_author 
-            WHERE
-            quote LIKE '%${keyword}%'
-        `;
-        
-        conn.query(sql, function (err, rows, fields) {
-        if (err) throw err;
-            res.send(rows);
-        });
+    // let keyword = req.query.searchTerm;
     
-    });
+    let rows = await getQuotes(req.query);
+    
+    res.send(rows);
 
-});//dbTest
+});//quotes
+
+app.get("/authorInfo", async function(req, res){
+    
+   let rows = await getAuthorInfo(req.query.authorId);
+  //res.render("quotes", {"records":rows});
+    res.send(rows)
+});//authorInfo
+
+// app.get();
+
+// app.post("/get", async function(req, res){
+    
+// });
+
+
+
+
+
+/* **********FUNCTIONS********** */
 
 //values in red must be updated
 function dbConnection(){
@@ -51,6 +63,130 @@ function dbConnection(){
 return conn;
 
 }
+
+
+function getAuthorInfo(authorId){
+    let conn = dbConnection();
+    
+    
+    return new Promise(function(resolve, reject){
+        conn.connect(function(err) {
+           if (err) throw err;
+           console.log("Connected!");
+        
+           let sql = `SELECT * 
+                      FROM q_author
+                      WHERE authorId = ${authorId}`;
+            console.log(sql);        
+           conn.query(sql, function (err, rows, fields) {
+              if (err) throw err;
+              //res.send(rows);
+              conn.end();
+              resolve(rows);
+           });
+        
+        });//connect
+    });//promise
+    
+}
+
+
+function getQuotes(query){
+    let keyword = query.searchTerm;
+    let conn = dbConnection();
+    
+    return new Promise(function(resolve, reject){
+        conn.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+            
+            let params = [];
+            
+            let sql = 
+                `SELECT * FROM q_quotes
+                NATURAL JOIN q_author 
+                WHERE
+                (quote LIKE '%${keyword}%'`;
+            
+            if (query.category) { //user selected a category
+                sql += " AND category = '" + query.category +"'"; //To prevent SQL injection, SQL statement shouldn't have any quotes.
+            }
+            
+            if (query.gender) { //user selected a sex
+                sql += " AND sex = '" + query.gender +"'"; //To prevent SQL injection, SQL statement shouldn't have any quotes.
+            }
+            
+            if (query.authorId) { //user selected a sex
+                sql += " AND authorId = " + query.authorId; //To prevent SQL injection, SQL statement shouldn't have any quotes.
+            }
+            
+            sql += ")";
+            
+            params.push(query.category);
+            params.push(query.gender);
+            params.push(query.authorId);
+            
+            console.log(params);
+            console.log(sql);
+            
+            conn.query(sql, params, function (err, rows, fields) {
+            if (err) throw err;
+            conn.end();
+            resolve(rows);
+            });
+        
+        });
+    });
+}
+
+function getAuthors(){
+    let conn = dbConnection();
+    
+    return new Promise(function(resolve, reject){
+        conn.connect(function(err) {
+           if (err) throw err;
+           console.log("Connected!");
+        
+           let sql = `SELECT authorId, firstName, lastName 
+                      FROM q_author
+                      ORDER BY authorId`;
+        
+           conn.query(sql, function (err, rows, fields) {
+              if (err) throw err;
+              //res.send(rows);
+              conn.end();
+              resolve(rows);
+           });
+        
+        });//connect
+    });//promise
+}
+
+function getCategories(){
+    
+    let conn = dbConnection();
+    
+    return new Promise(function(resolve, reject){
+        conn.connect(function(err) {
+           if (err) throw err;
+           console.log("Connected!");
+        
+           let sql = `SELECT DISTINCT category 
+                      FROM q_quotes
+                      ORDER BY category`;
+        
+           conn.query(sql, function (err, rows, fields) {
+              if (err) throw err;
+              //res.send(rows);
+              conn.end();
+              resolve(rows);
+           });
+        
+        });//connect
+    });//promise
+    
+}
+
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
